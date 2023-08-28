fcd77af40b9548b28f6b30a877c972b171d303bc
ricardo <ricardorbxx1@gmail.com>
2023-08-28 12:16:15 -0300

    Estilizei o header e troquei o favicon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,7 @@ import { AiOutlineStar, AiOutlineSearch } from 'react-icons/ai';
 import { CgProfile } from 'react-icons/cg';
 import { getUser } from '../services/userAPI';
 import type { UserType } from '../types';
+import trybeTunesLogo from '../images/trybetunes_logo.svg';
 
 function Header() {
   const [userInfo, setUserInfo] = useState<UserType | null>(null);
@@ -20,15 +21,20 @@ function Header() {
   }, []);
 
   return (
-    <>
+    <div className="flex">
       <header
         data-testid="header-component"
-        className="bg-red-500 text-3xl"
+        className="text-lg h-screen flex flex-col
+        justify-between items-center px-12 shadow-lg"
       >
-        <div>
+        <div className="mt-3">
+          <img src={ trybeTunesLogo } alt="trybetunes logo" className="h-32" />
+        </div>
+        <nav className="flex flex-col gap-6">
           <NavLink
             data-testid="link-to-search"
             to="/search"
+            className="flex text-3xl items-center gap-2 text-gray-400"
           >
             <AiOutlineSearch />
             {' '}
@@ -37,6 +43,7 @@ function Header() {
           <NavLink
             data-testid="link-to-favorites"
             to="/favorites"
+            className="flex text-3xl items-center gap-2 text-gray-400"
           >
             <AiOutlineStar />
             {' '}
@@ -45,22 +52,37 @@ function Header() {
           <NavLink
             data-testid="link-to-profile"
             to="/profile"
+            className="flex text-3xl items-center gap-2 text-gray-400"
           >
             <CgProfile />
             {' '}
             <span>Perfil</span>
           </NavLink>
-        </div>
+        </nav>
         <div>
-          {isLoading ? (<p>Carregando...</p>) : (
-            <p data-testid="header-user-name">{userInfo?.name}</p>
+          {isLoading ? (
+            <p className="flex text-2xl items-center mb-6">Carregando...</p>
+          ) : (
+            <div className="flex my-auto  mb-6 gap-2">
+              {userInfo?.image && (
+                <img
+                  src={ userInfo.image }
+                  alt="small face of the user"
+                  className="w-12 h-12 rounded-full"
+                />
+              )}
+              <p
+                data-testid="header-user-name"
+                className="flex text-2xl items-center"
+              >
+                {userInfo?.name}
+              </p>
+            </div>
           )}
         </div>
       </header>
-      <div>
-        <Outlet />
-      </div>
-    </>
+      <Outlet />
+    </div>
   );
 }
 
